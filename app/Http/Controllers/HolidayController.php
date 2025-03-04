9beea048096e12e9889b53510a7cd54ef3dcdb45
Carl Anthony Magumpara <carl@lexmeet.com>
2025-03-04 19:44:14 +0800

    updates

--- app/Http/Controllers/HolidayController.php
+++ app/Http/Controllers/HolidayController.php
@@ -9,7 +9,14 @@ class HolidayController extends Controller
 {
     public function index(Request $request)
     {
-        $holiday = Holiday::orderBy('date', 'DESC');
+        if($request->has('order_by') && $request->order_by && !in_array($request->order_by, ['ASC', 'DESC'])) {
+            return response()->json([
+                'result' => 'error',
+                'message' => 'The provided order_by should ASC or DESC only.',
+            ], 200);
+        }
+
+        $holiday = Holiday::orderBy('date', $request->order_by ?? 'DESC');
 
         if($request->has('year') && $request->year) {
             $holiday->whereYear('date', $request->year);
@@ -17,6 +24,9 @@ class HolidayController extends Controller
             $holiday->whereYear('date', date('Y'));
         }
 
-        return $holiday->get();
+        return response()->json([
+            'result' => 'success',
+            'data' => $holiday->get()
+        ], 200);
     }
 }
